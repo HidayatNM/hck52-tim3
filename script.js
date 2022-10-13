0ceca1cbafe2213b53aad4173bf1c663c145acf6
HidayatNM <hidayatnurmusa1@gmail.com>
2022-10-13 18:40:45 +0700

    update js script

--- script.js
+++ script.js
@@ -1,30 +1,95 @@
 const data = [
   {
-    namaSepatu: "",
-    brandSepatu: "",
-    image: "",
-    quantity: 0,
-    harga: 0,
+    namaSepatu: "Converse Chuck 70 Josh Vides White",
+    brandSepatu: "Converse",
+    image: "images/Converse Chuck 70 Josh Vides White/Converse-Chuck-70-Josh-Vides-White-2.jpg",
+    quantity: 10,
+    harga: 190300,
   },
   {
-    namaSepatu: "",
-    brandSepatu: "",
-    image: "",
-    quantity: 0,
-    harga: 0,
+    namaSepatu: "Converse Chuck Taylor All Star 70 Hi Basquiat Kings Of Egipt III",
+    brandSepatu: "Converse",
+    image: "images/Converse Chuck Taylor All-Star 70 Hi Basquiat Kings of Egypt III/Converse-Chuck Taylor-All-Star-70-Hi-BasquiatKingsofEgypt-III-2.jpg",
+    quantity: 10,
+    harga: 190300,
   },
   {
-    namaSepatu: "",
-    brandSepatu: "",
-    image: "",
-    quantity: 0,
-    harga: 0,
+    namaSepatu: "Converse Chuck Taylor All Star 70 Hi Keith Haring Egret",
+    brandSepatu: "Converse",
+    image: "images/Converse Chuck Taylor All-Star 70 Hi Keith Haring Egret/Converse-Chuck-Taylor-All-Star-70-Hi-Keith-Haring-Egret.jpg",
+    quantity: 10,
+    harga: 190300,
   },
   {
-    namaSepatu: "",
-    brandSepatu: "",
-    image: "",
-    quantity: 0,
-    harga: 0,
+    namaSepatu: "Converse Chuck Taylor All Star Basquiat",
+    brandSepatu: "Converse",
+    image: "images/Converse Chuck Taylor All-Star Basquiat Pez Dispenser Dinosaur/Converse-Chuck-Taylor-All-Star-Basquiat-Pez-Dispenser-Dinosaur.jpg",
+    quantity: 10,
+    harga: 190300,
+  },
+  {
+    namaSepatu: "New Balance 530",
+    brandSepatu: "New Balance",
+    image: "images/gambar-1/new-belance-1.webp",
+    quantity: 10,
+    harga: 190300,
+  },
+  {
+    namaSepatu: "New Balance Dark",
+    brandSepatu: "New Balance",
+    image: "images/gambar-2/new-belance-drak-1.webp",
+    quantity: 10,
+    harga: 190300,
+  },
+  {
+    namaSepatu: "New Balance 550",
+    brandSepatu: "New Balance",
+    image: "images/gambar-3/new-belance-white-3.webp",
+    quantity: 10,
+    harga: 190300,
+  },
+  {
+    namaSepatu: "New Balance Dark 230",
+    brandSepatu: "New Balance",
+    image: "images/gambar-2/new-belance-drak-1.webp",
+    quantity: 10,
+    harga: 190300,
+  },
+  {
+    namaSepatu: "Nike Air Force 1",
+    brandSepatu: "Nike",
+    image: "images/Nike-Air-Force-1-Women/1adiAir1.jpg",
+    quantity: 10,
+    harga: 190300,
+  },
+  {
+    namaSepatu: "Air Jordan 4",
+    brandSepatu: "Nike",
+    image: "images/Nike-Air-Jordan/1airJordan1.jpg",
+    quantity: 10,
+    harga: 190300,
+  },
+  {
+    namaSepatu: "Air Max 90",
+    brandSepatu: "Nike",
+    image: "images/Nike-Air-Max-90/1airMax1.jpg",
+    quantity: 10,
+    harga: 190300,
+  },
+  {
+    namaSepatu: "Nike Dunk",
+    brandSepatu: "Nike",
+    image: "images/Nike-Air-Max-90/3airMax3.jpg",
+    quantity: 10,
+    harga: 190300,
   },
 ];
+
+let getName = document.getElementsByClassName('card-title')
+let getPrice = document.getElementsByClassName('card-price')
+let getImg = document.getAttribute('src')
+
+for (let i = 0; i < data.length; i++) {
+  const element = data[i];
+  
+}
