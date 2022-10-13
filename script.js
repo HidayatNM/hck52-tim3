22319709870f31d95af915351599b73edec189da
januaricks <muhammadjanu30@gmail.com>
2022-10-13 20:44:57 +0700

    add cart

--- script.js
+++ script.js
@@ -1,30 +1,4 @@
-const data = [
-  {
-    namaSepatu: "",
-    brandSepatu: "",
-    image: "",
-    quantity: 0,
-    harga: 0,
-  },
-  {
-    namaSepatu: "",
-    brandSepatu: "",
-    image: "",
-    quantity: 0,
-    harga: 0,
-  },
-  {
-    namaSepatu: "",
-    brandSepatu: "",
-    image: "",
-    quantity: 0,
-    harga: 0,
-  },
-  {
-    namaSepatu: "",
-    brandSepatu: "",
-    image: "",
-    quantity: 0,
-    harga: 0,
-  },
-];
+
+
+
+
